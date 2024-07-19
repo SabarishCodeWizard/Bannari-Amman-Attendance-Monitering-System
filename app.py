8d6790466f0fc0327e05a67083fbe12acfe56d7a
Sabarish111111111 <sabarish.it22@bitsathy.ac.in>
2024-07-19 15:59:48 +0530

    audio

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 import cv2
 import os
-from flask import Flask,request,render_template
-from datetime import date
-from datetime import datetime
+from flask import Flask, request, render_template
+from datetime import date, datetime
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import joblib
+from gtts import gTTS
+from io import BytesIO
+import pygame
 
 #### Defining Flask App
 app = Flask(__name__)
-
 
 #### Saving Date today in 2 different formats
 datetoday = date.today().strftime("%m_%d_%y")
 datetoday2 = date.today().strftime("%d-%B-%Y")
 
-
 #### Initializing VideoCapture object to access WebCam
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 
 #### If these directories don't exist, create them
 if not os.path.isdir('Attendance'):
@@ -32,16 +31,14 @@ if f'Attendance-{datetoday}.csv' not in os.listdir('Attendance'):
     with open(f'Attendance/Attendance-{datetoday}.csv','w') as f:
         f.write('Name,Roll,Time')
 
-
 #### get a number of total registered users
 def totalreg():
     return len(os.listdir('static/faces'))
 
-
 #### extract the face from an image
 def extract_faces(img):
     try:
-        if img.shape!=(0,0,0):
+        if img.shape != (0, 0, 0):
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             face_points = face_detector.detectMultiScale(gray, 1.3, 5)
             return face_points
@@ -49,12 +46,11 @@ def extract_faces(img):
             return []
     except:
         return []
-    
+
 #### Identify face using ML model
 def identify_face(facearray):
     model = joblib.load('static/face_recognition_model.pkl')
     return model.predict(facearray)
-
 
 #### A function which trains the model on all the faces available in faces folder
 def train_model():
@@ -69,9 +65,8 @@ def train_model():
             labels.append(user)
     faces = np.array(faces)
     knn = KNeighborsClassifier(n_neighbors=5)
-    knn.fit(faces,labels)
-    joblib.dump(knn,'static/face_recognition_model.pkl')
-
+    knn.fit(faces, labels)
+    joblib.dump(knn, 'static/face_recognition_model.pkl')
 
 #### Extract info from today's attendance file in attendance folder
 def extract_attendance():
@@ -80,8 +75,7 @@ def extract_attendance():
     rolls = df['Roll']
     times = df['Time']
     l = len(df)
-    return names,rolls,times,l
-
+    return names, rolls, times, l
 
 #### Add Attendance of a specific user
 def add_attendance(name):
@@ -91,9 +85,23 @@ def add_attendance(name):
     
     df = pd.read_csv(f'Attendance/Attendance-{datetoday}.csv')
     if int(userid) not in list(df['Roll']):
-        with open(f'Attendance/Attendance-{datetoday}.csv','a') as f:
+        with open(f'Attendance/Attendance-{datetoday}.csv', 'a') as f:
             f.write(f'\n{username},{userid},{current_time}')
+        return True
+    return False
 
+#### Function to play voice message
+def play_voice_message(message):
+    tts = gTTS(text=message, lang='en')
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load(fp)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
 
 def getallusers():
     userlist = os.listdir('static/faces')
@@ -102,11 +110,11 @@ def getallusers():
     l = len(userlist)
 
     for i in userlist:
-        name,roll = i.split('_')
+        name, roll = i.split('_')
         names.append(name)
         rolls.append(roll)
     
-    return userlist,names,rolls,l
+    return userlist, names, rolls, l
 
 def deletefolder(duser):
     pics = os.listdir(duser)
@@ -121,69 +129,68 @@ def deletefolder(duser):
 #### Our main page
 @app.route('/')
 def home():
-    names,rolls,times,l = extract_attendance()    
-    return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2)  
-
+    names, rolls, times, l = extract_attendance()    
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2)  
 
 #### This function will run when we click on Take Attendance Button
-@app.route('/start',methods=['GET'])
+@app.route('/start', methods=['GET'])
 def start():
     if 'face_recognition_model.pkl' not in os.listdir('static'):
-        return render_template('home.html',totalreg=totalreg(),datetoday2=datetoday2,mess='There is no trained model in the static folder. Please add a new face to continue.') 
+        return render_template('home.html', totalreg=totalreg(), datetoday2=datetoday2, mess='There is no trained model in the static folder. Please add a new face to continue.')
 
     ret = True
     cap = cv2.VideoCapture(0)
     while ret:
-        ret,frame = cap.read()
-        if len(extract_faces(frame))>0:
-            (x,y,w,h) = extract_faces(frame)[0]
-            cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
-            face = cv2.resize(frame[y:y+h,x:x+w], (50, 50))
-            identified_person = identify_face(face.reshape(1,-1))[0]
-            add_attendance(identified_person)
-            cv2.putText(frame,f'{identified_person}',(30,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
-        cv2.imshow('Attendance',frame)
-        if cv2.waitKey(1)==27:
+        ret, frame = cap.read()
+        if len(extract_faces(frame)) > 0:
+            (x, y, w, h) = extract_faces(frame)[0]
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 20), 2)
+            face = cv2.resize(frame[y:y+h, x:x+w], (50, 50))
+            identified_person = identify_face(face.reshape(1, -1))[0]
+            if add_attendance(identified_person):
+                # Play voice message
+                play_voice_message('Attendance marked successfully.')
+            cv2.putText(frame, f'{identified_person}', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
+        cv2.imshow('Attendance', frame)
+        if cv2.waitKey(1) == 27:
             break
     cap.release()
     cv2.destroyAllWindows()
-    names,rolls,times,l = extract_attendance()    
-    return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2) 
-
+    names, rolls, times, l = extract_attendance()    
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2) 
 
 #### This function will run when we add a new user
-@app.route('/add',methods=['GET','POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     newusername = request.form['newusername']
     newuserid = request.form['newuserid']
     userimagefolder = 'static/faces/'+newusername+'_'+str(newuserid)
     if not os.path.isdir(userimagefolder):
         os.makedirs(userimagefolder)
-    i,j = 0,0
+    i, j = 0, 0
     cap = cv2.VideoCapture(0)
     while 1:
-        _,frame = cap.read()
+        _, frame = cap.read()
         faces = extract_faces(frame)
-        for (x,y,w,h) in faces:
-            cv2.rectangle(frame,(x, y), (x+w, y+h), (255, 0, 20), 2)
-            cv2.putText(frame,f'Images Captured: {i}/50',(30,30),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 20),2,cv2.LINE_AA)
-            if j%10==0:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 20), 2)
+            cv2.putText(frame, f'Images Captured: {i}/50', (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 20), 2, cv2.LINE_AA)
+            if j % 10 == 0:
                 name = newusername+'_'+str(i)+'.jpg'
-                cv2.imwrite(userimagefolder+'/'+name,frame[y:y+h,x:x+w])
-                i+=1
-            j+=1
-        if j==500:
+                cv2.imwrite(userimagefolder+'/'+name, frame[y:y+h, x:x+w])
+                i += 1
+            j += 1
+        if j == 500:
             break
-        cv2.imshow('Adding new User',frame)
-        if cv2.waitKey(1)==27:
+        cv2.imshow('Adding new User', frame)
+        if cv2.waitKey(1) == 27:
             break
     cap.release()
     cv2.destroyAllWindows()
     print('Training Model')
     train_model()
-    names,rolls,times,l = extract_attendance()    
-    return render_template('home.html',names=names,rolls=rolls,times=times,l=l,totalreg=totalreg(),datetoday2=datetoday2) 
-
+    names, rolls, times, l = extract_attendance()    
+    return render_template('home.html', names=names, rolls=rolls, times=times, l=l, totalreg=totalreg(), datetoday2=datetoday2) 
 
 #### Our main function which runs the Flask App
 if __name__ == '__main__':
